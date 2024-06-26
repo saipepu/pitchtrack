7ef95fe9c010718e2a5386793c6c3aa4a26994c5
saipepu <saipepu.mdy257@gmail.com>
2024-06-26 23:56:16 +0700

    Created: Context Hook

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,25 @@
-import React, { ReactNode } from 'react'
+"use client";
+
+import React, { ReactNode, createContext, useState } from 'react'
+import slotList from '@/utils/dummy-data/slots'
+
+export const SlotContext = createContext<any>(null)
 
 const layout = ({
     children
 } : {
     children: ReactNode
 }) => {
+
+  const [slots, setSlots] = useState(slotList)
+  console.log(slots)
+
   return (
-    <div className='w-full h-full overflow-hidden'>
+    <SlotContext.Provider value={{slots, setSlots}}>
+      <div className='w-full h-full overflow-hidden'>
         {children}
-    </div>
+      </div>
+    </SlotContext.Provider>
   )
 }
 
