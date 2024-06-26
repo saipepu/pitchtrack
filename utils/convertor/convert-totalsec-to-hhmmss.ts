fd76c82fb6feaad11c18349514138cac96f29a11
saipepu <saipepu.mdy257@gmail.com>
2024-06-27 00:00:06 +0700

    Modified

--- utils/convertor/convert-totalsec-to-hhmmss.ts
+++ utils/convertor/convert-totalsec-to-hhmmss.ts
@@ -3,8 +3,9 @@ export const convertTotalSectoHHMMSS = (totalSeconds: number) => {
     let minutes: number | string = Math.floor((totalSeconds - (hours * 3600)) / 60);
     let seconds: number | string = totalSeconds - (hours * 3600) - (minutes * 60);
   
+    if (hours < 10) { hours = hours; }
     if (minutes < 10) { minutes = minutes; }
     if (seconds < 10) { seconds = "0" + seconds; }
   
-    return minutes + ':' + seconds;
+    return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds;
 }
