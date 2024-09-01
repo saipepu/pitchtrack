65757ecbfe73a227d78d106860252f7b766e179c
saipepu <saipepu.mdy257@gmail.com>
2024-09-01 14:25:35 +0700

    update api url I promise. this is last time

--- app/_api/api.js
+++ app/_api/api.js
@@ -1,6 +1,6 @@
 // azure server api url
-export const api = 'http://20.2.64/76/api'
-export const socketApi = 'http://20.2.64/76'
+export const api = 'http://20.2.64.76/api'
+export const socketApi = 'http://20.2.64.76'
 
 // export const api = 'http://localhost:8080/api'
 // export const socketApi = 'http://localhost:8080'
