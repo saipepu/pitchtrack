846f51056acf8be13fe61fdf9a62caf982758ca8
saipepu <saipepu.mdy257@gmail.com>
2024-09-01 13:47:00 +0700

    updated api url

--- app/_api/api.js
+++ app/_api/api.js
@@ -1,5 +1,3 @@
 // azure server api url
 export const api = process.env.NEXT_PUBLIC_API_URL || 'https://**.**.**.**'
 export const socketApi = process.env.NEXT_PUBLIC_SOCKET_URL || 'https://**.**.**.**'
-export const localApi = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api'
-export const localSocketApi = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:8080'
