c805c555235ec285f5ad9572dc6285659144436e
saipepu <saipepu.mdy257@gmail.com>
2024-09-10 14:38:29 +0700

    Handle New API dto

--- app/[eventId]/dashboard/_components/PlayButton/PlayButton.tsx
+++ app/[eventId]/dashboard/_components/PlayButton/PlayButton.tsx
@@ -2,38 +2,46 @@ import React from 'react'
 import { EmitSocket } from '@/utils/socket/SocketEmit'
 import { Pause, Play } from 'lucide-react'
 
-const PlayButton = ({ slot, eventId } : any ) => {
+const PlayButton = ({ slot, eventId, isRunning, setIsRunning, isActive } : any ) => {
 
   const handlePlayButton = () => {
-    switch(slot?.status) {
-      case 'active':
-        EmitSocket('pause', {
-          duration: slot.duration,
-          eventId: eventId,
-          slotId: slot._id
-        })
-        break
-      case 'paused':
-        EmitSocket('resume', {
-          duration: slot.duration,
-          eventId: eventId,
-          slotId: slot._id
-        })
-        break
-      default:
-        EmitSocket('start', {
-          duration: slot.duration,
-          eventId: eventId,
-          slotId: slot._id
-        })
+    console.log('isRunning', isRunning, 'isActive', isActive)
+    if(isActive && isRunning) {
+      EmitSocket('pause', {
+        duration: slot.duration,
+        eventId: eventId,
+        slotId: slot._id
+      })
+      setIsRunning(false)
+    } else if(isActive) {
+      EmitSocket('resume', {
+        duration: slot.duration,
+        eventId: eventId,
+        slotId: slot._id
+      })
+    } else {
+      EmitSocket('start', {
+        duration: slot.duration,
+        eventId: eventId,
+        slotId: slot._id
+      })
     }
   }
+
   return (
     <div
-      className={`group cursor-pointer w-full h-full flex justify-center items-center gap-[2px] rounded-md border-[1px] border-slate-300 px-1 md:px-2 ${slot?.status == 'active' ? 'bg-red-300' : 'bg-slate-100 lg:hover:bg-green-400'} transition-all duration-300`}
+      className={
+        `group cursor-pointer w-full h-full flex justify-center items-center gap-[2px] rounded-md border-[1px] border-slate-300 px-1 md:px-2
+        ${isRunning && isActive ? 'bg-slate-500' : isActive ? 'bg-green-500' : 'bg-transparent'}
+        transition-all duration-300`
+      }
       onClick={() => handlePlayButton()}
     >
-      {slot?.status == 'active' ? <Pause size={16} fill={"white"} stroke={"white"} /> : <Play size={16} fill="black" strokeWidth="0" className='lg:group-hover:fill-white lg:group-hover:stroke-white' /> }
+      {isRunning && isActive ? (
+        <Pause size={16} fill={"white"} stroke={"white"} />
+      ) : (
+        <Play size={16} fill={isActive ? "white" : "black"} strokeWidth="0" className='lg:group-hover:stroke-white' />
+      )}
     </div>
   )
 }
