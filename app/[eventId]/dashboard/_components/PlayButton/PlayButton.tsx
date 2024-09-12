fdeb72456b3f2d44909fab946513fcfaa4ffb178
saipepu <saipepu.mdy257@gmail.com>
2024-09-13 02:39:24 +0700

    Handle Timer Complete

--- app/[eventId]/dashboard/_components/PlayButton/PlayButton.tsx
+++ app/[eventId]/dashboard/_components/PlayButton/PlayButton.tsx
@@ -1,39 +1,63 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import { EmitSocket } from '@/utils/socket/SocketEmit'
 import { Pause, Play } from 'lucide-react'
+import socket from '@/utils/socket'
 
 const PlayButton = ({ slot, eventId, isRunning, setIsRunning, isActive, setIsLoading } : any ) => {
 
+  const [isCompleted, setIsCompleted] = useState(false)
+
   const handlePlayButton = () => {
     setIsLoading && setIsLoading(true)
-    if(isActive && isRunning) {
-      EmitSocket('pause', {
-        duration: slot.duration,
-        eventId: eventId,
-        slotId: slot._id
-      })
-      setIsRunning(false)
-    } else if(isActive) {
-      EmitSocket('resume', {
-        duration: slot.duration,
-        eventId: eventId,
-        slotId: slot._id
-      })
-      setIsRunning(true)
-    } else {
+
+    if(isCompleted) {
+      setIsCompleted(false)
       EmitSocket('start', {
         duration: slot.duration,
         eventId: eventId,
         slotId: slot._id
       })
       setIsRunning(true)
+
+    } else {
+
+      if(isActive && isRunning) {
+        EmitSocket('pause', {
+          duration: slot.duration,
+          eventId: eventId,
+          slotId: slot._id
+        })
+        setIsRunning(false)
+      } else if(isActive) {
+        EmitSocket('resume', {
+          duration: slot.duration,
+          eventId: eventId,
+          slotId: slot._id
+        })
+        setIsRunning(true)
+      } else {
+        EmitSocket('start', {
+          duration: slot.duration,
+          eventId: eventId,
+          slotId: slot._id
+        })
+        setIsRunning(true)
+      }
+
     }
   }
+
+  socket.on('timerCompleted', () => {
+    setIsRunning(false)
+    setIsCompleted(true)
+  })
 
   return (
     <div
       className={
-        `z-50 group cursor-pointer w-full h-full flex justify-center items-center gap-[2px] rounded-md border-[1px] border-slate-300 px-1 md:px-2
+        `z-30 group cursor-pointer w-full h-full flex justify-center items-center gap-[2px] rounded-md border-[1px] border-slate-300 px-1 md:px-2
         ${isRunning && isActive ? 'bg-slate-500' : isActive ? 'bg-green-500' : 'bg-transparent'}
         transition-all duration-300`
       }
