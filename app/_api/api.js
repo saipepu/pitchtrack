8a982c77502a8cdc27d195e7e5cd090f1273e34b
saipepu <saipepu.mdy257@gmail.com>
2024-09-26 09:54:10 +0700

    api route

--- app/_api/api.js
+++ app/_api/api.js
@@ -1,6 +1,6 @@
 // DYNAMIC API URL
-// export const api = '/api'
-// export const socketApi = ''
+export const api = '/api'
+export const socketApi = ''
 
 // LOCAL API URL
 // export const api = 'http://localhost:8080/api'
@@ -11,5 +11,5 @@
 // export const socketApi = 'http://pitchtrack.freaks.com'
 
 // AZURE API URL
-export const api = 'http://20.2.64.76/api'
-export const socketApi = 'http://20.2.64.76'
+// export const api = 'http://20.2.64.76/api'
+// export const socketApi = 'http://20.2.64.76'
