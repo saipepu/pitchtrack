82333b0441f2bcc3ca32dd99c64decf7a96e424c
saipepu <saipepu.mdy257@gmail.com>
2024-10-15 22:02:32 +0700

    All Mighty Push

--- app/_api/api.js
+++ app/_api/api.js
@@ -1,10 +1,10 @@
 // DYNAMIC API URL
-// export const api = '/api'
-// export const socketApi = ''
+export const api = '/api'
+export const socketApi = ''
 
 // LOCAL API URL
-export const api = 'http://localhost:8080/api'
-export const socketApi = 'http://localhost:8080'
+// export const api = 'http://localhost:8080/api'
+// export const socketApi = 'http://localhost:8080'
 
 // SUBDOMAIN API URL
 // export const api = 'https://pitchtrack.freaks.com/api'
